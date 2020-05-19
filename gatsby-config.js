8aba16f1cf712ee1e5351486ce4153b78603d663
Nick Akey <nicktheflanders@gmail.com>
2020-05-19 08:47:15 -0500

    updated title and description

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Remember Spanish Words',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Tools, tricks, and mnemonics to memorize Spanish words',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
